62817d4f7365575c74747690347b3b39cb9791e2
administrator@MBC.EDU.MO <administrator@MBC.EDU.MO>
2020-09-30 14:27:14 +0800

    fix f1.js for ans fra

--- package.bat
+++ package.bat
@@ -1,13 +1,16 @@
 npm i body-parser
-npm i connect-redis
 npm i cookie-parser
 npm i express
 npm i express-session
+npm i connect-redis
+npm i passport
+npm i passport-local
 npm i morgan
 npm i multer
 npm i mysql
 npm i nconf
-npm i passport
-npm i passport-local
 npm i pug
 npm i redis
+npm i -g typescirpt
+npm i @types/node
+
