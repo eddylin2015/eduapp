e90a4882545eee0b2528a2eac1effb6a5c19b732
unknown <eddylin2015@gmail.com>
2020-10-03 12:15:21 +0800

    actreg constructing

---
+++ routes/actreg/crud.js
@@ -0,0 +1,359 @@
+// Copyright 2017, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const express = require('express');
+const images = require('./images');
+
+function getModel () {
+    return require(`./model-mysql-pool`);
+}
+function fmt_title(username, datestr, description) {
+    description = description.split("\n")[0];
+    //description = description.length > 10 ? description.substring(0, 10) : description;
+    datestr = datestr.length > 10 ? datestr.substring(0, 10) : datestr;
+    return username + ":" + datestr + ":" + description;
+}
+function fmt_now() {
+    var d = new Date();
+    var dstr = d.getFullYear() + "-";
+    if (d.getMonth() < 10) dstr += "0";
+    dstr += d.getMonth()+1 + "-";
+    if (d.getDate() < 10) dstr += "0";
+    dstr += d.getDate();
+    return dstr;
+}
+function checkuser(req){
+	if(req.user.email=="lammou@mail.mbc.edu.mo") return true;
+	if(req.user.email=="joe853.hong@mail.mbc.edu.mo") return true;
+	return false;
+}
+const router = express.Router();
+// Use the oauth middleware to automatically get the user's profile
+// information and expose login/logout URLs to templates.
+// Set Content-Type for all responses for these routes
+router.use((req, res, next) => {
+  res.set('Content-Type', 'text/html');
+  next();
+});
+/**
+ * GET /books/add
+ *
+ * Display a page of books (up to ten at a time).
+ */
+router.get('/', (req, res, next) => {
+  res.end("constructing");
+  return;
+	if(!checkuser(req)){ res.end("no right");return;}
+  getModel().list(req.user.id, 10, req.query.pageToken, (err, entities, cursor) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    //res.render('actreg/list.pug', {
+    res.render('actreg/list.pug', {
+      profile: req.user ,
+      books: entities,
+      nextPageToken: cursor
+    });
+  });
+});
+// Use the oauth2.required middleware to ensure that only logged-in users
+// can access this handler.
+router.get('/mine', require('connect-ensure-login').ensureLoggedIn(), (req, res, next) => {
+  if(!checkuser(req)){ res.end("no right");return;}
+  getModel().listBy(
+    req.user.id,
+    10,
+    req.query.pageToken,
+    (err, entities, cursor, apiResponse) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.render('actreg/list.pug', {
+        profile: req.user,
+        books: entities,
+        nextPageToken: cursor
+      });
+    }
+  );
+});
+router.post('/:book/imageUploader', images.multer.any(),   function(req, res) {
+    //req.file/req.files
+	res.send({
+		"uploaded": 1,
+    	"fileName": "IMAGE.PNG",
+    	"url": "/ckeditorimages/"+req.files[0].filename
+	})
+})
+router.get('/searchform', (req, res) => {
+	if(!checkuser(req)){ res.end("no right");return;}
+    res.render('actreg/searchform.pug', {
+        profile: req.user,
+        book: {
+            author: req.user.username,
+            logDate: fmt_now(),
+            title:""
+        },
+        action: 'Post'
+    });
+});
+router.post('/searchform', require('connect-ensure-login').ensureLoggedIn(),
+    images.multer.single('image'),
+    (req, res) => {
+		if(!checkuser(req)){ res.end("no right");return;}
+        const data = req.body;
+        var author = data.author;
+    getModel().listTimestampBy(
+        req.user.id,
+        author,
+        data.slogDate,
+        data.elogDate,
+        10,
+        req.query.pageToken,
+        (err, entities, cursor, apiResponse) => {
+            if (err) {
+                next(err);
+                return;
+            }
+            res.render('actreg/table.pug', {
+                profile: req.user,
+                books: entities,
+                nextPageToken: cursor
+            });
+        }
+    );
+});
+/**
+ * GET /books/add
+ *
+ * Display a form for creating a book.
+ */
+router.get('/add', (req, res) => {
+    if(!checkuser(req)){ res.end("no right");return;}
+    let formpug='actreg/form.pug';
+    if(req.query.ckedit) formpug='actreg/form.1.pug';
+    res.render(formpug, {
+        profile: req.user,
+        book: {
+            author: req.user.username,
+            authorname: req.user.displayName,
+            logDate: fmt_now(),
+            rootid: 0,
+            title: fmt_title(req.user.username, fmt_now(), 'worklog'),
+            createdById: req.user.id,
+            deptlog: 0
+        },
+        action: 'Add'
+    });
+});
+router.get('/:book/follow', (req, res) => {
+	if(!checkuser(req)){ res.end("no right");return;}
+    res.render('actreg/form.pug', {
+        profile: req.user,
+        book: {
+            deptlog: 0,
+            author: req.user.username,
+            authorname: req.user.displayName,
+            logDate: fmt_now(),
+            parentid: req.params.book,
+            rootid: req.query.rid,
+            title: fmt_title(req.user.username, fmt_now(), 'worklog'),
+            description: req.query.t,
+            createdById : req.user.id
+        },
+        action: 'Follow'
+    });
+});
+router.post(
+    '/:book/follow',
+    images.multer.single('image'),
+    (req, res, next) => {
+		if(!checkuser(req)){ res.end("no right");return;}
+        const data = req.body;        
+        // If the user is logged in, set them as the creator of the book.
+        if (req.user) {
+            data.createdBy = req.user.displayName;
+            data.createdById = req.user.id;
+        } else {
+            data.createdBy = 'Anonymous';
+        }
+        if (data.rootid > 0 && data.deptlog !== 0) { getModel().updateGroupStatus(data.rootid, data.deptlog); }
+        if (data.deptlog == 1) data.deptlog = 2;
+        // Was an image uploaded? If so, we'll use its public URL
+        // in cloud storage.
+        // Save the data to the database.
+        data.title = fmt_title(data.author, data.logDate, data.description)
+        getModel().create(req.user.id, data, (err, savedData) => {
+            if (err) {
+                next(err);
+                return;
+            }
+            res.redirect(`${req.baseUrl}/${savedData.id}`);
+        });
+    }
+);
+router.get('/followlist', (req, res) => {
+	if(!checkuser(req)){ res.end("no right");return;}
+    getModel().listByParentid(
+        req.user.id,
+        req.query.rid,
+        10,
+        req.query.pageToken,
+        (err, entities, cursor, apiResponse) => {
+            if (err) {
+                next(err);
+                return;
+            }
+            res.render('actreg/table.pug', {
+                profile: req.user,
+                books: entities,
+                nextPageToken: cursor
+            });
+        }
+    );
+});
+
+/**
+ * POST /books/add
+ *
+ * Create a book.
+ */
+// [START add]
+router.post(
+    '/add',
+    images.multer.single('image'),
+  (req, res, next) => {
+	  if(!checkuser(req)){ res.end("no right");return;}
+     const data = req.body;
+    // If the user is logged in, set them as the creator of the book.
+    if (req.user) {
+      data.createdBy = req.user.displayName;
+      data.createdById = req.user.id;
+    } else {
+      data.createdBy = 'Anonymous';
+     }
+    if (data.deptlog == 1) data.deptlog = 2;
+    // Was an image uploaded? If so, we'll use its public URL
+    // in cloud storage.
+    // Save the data to the database.
+    data.title = fmt_title(data.author, data.logDate, data.description)
+    getModel().create(req.user.id, data, (err, savedData) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        res.redirect(`${req.baseUrl}/${savedData.id}`);
+    });
+  }
+);
+// [END add]
+
+/**
+ * GET /books/:id/edit
+ *
+ * Display a book for editing.
+ */
+router.get('/:book/edit', (req, res, next) => {
+	if(!checkuser(req)){ res.end("no right");return;}
+    getModel().read(req.user.id, req.params.book, (err, entity) => {
+    if (err) {
+      next(err);
+      return;
+      }    
+      let formpug='actreg/form.pug';
+      if(req.query.ckedit) formpug='actreg/form.1.pug';
+    res.render(formpug, {
+      profile: req.user,
+      book: entity,
+      action: 'Edit'
+    });
+  });
+});
+
+/**
+ * POST /books/:id/edit
+ *
+ * Update a book.
+ */
+router.post(
+    '/:book/edit',
+    images.multer.single('image'), require('connect-ensure-login').ensureLoggedIn(),
+    (req, res, next) => {
+		if(!checkuser(req)){ res.end("no right");return;}
+    const data = req.body;
+    // Was an image uploaded? If so, we'll use its public URL
+    // in cloud storage.
+    //if (req.file && req.file.cloudStoragePublicUrl) {
+    //  req.body.imageUrl = req.file.cloudStoragePublicUrl;
+    //}
+    if (data.rootid > 0 && data.deptlog !== 0) { getModel().updateGroupStatus(data.rootid, data.deptlog); }
+    if (data.deptlog==1) data.deptlog = 2;
+        
+    data.title = fmt_title(data.author, data.logDate, data.description);
+    getModel().update(req.user.id,req.params.book, data, (err, savedData) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.redirect(`${req.baseUrl}/${savedData.id}`);
+    });
+  }
+);
+/**
+ * GET /books/:id
+ *
+ * Display a book.
+ */
+router.get('/:book', (req, res, next) => {
+if(!checkuser(req)){ res.end("no right");return;}	
+
+  getModel().read(req.user.id, req.params.book, (err, entity) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.render('actreg/view.pug', {
+      profile: req.user,
+      book: entity
+    });
+  });
+});
+/**
+ * GET /books/:id/delete
+ *
+ * Delete a book.
+ */
+router.get('/:book/delete', (req, res, next) => {
+	if(!checkuser(req)){ res.end("no right");return;}
+    getModel().delete(req.user.id,req.params.book, (err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.redirect(req.baseUrl);
+  });
+});
+
+/**
+ * Errors on "/books/*" routes.
+ */
+router.use((err, req, res, next) => {
+  // Format error and forward to generic error handler for logging and
+  // responding to the request
+  err.response = err.message;
+  next(err);
+});
+module.exports = router;
---
+++ routes/actreg/model-mysql-pool.js
@@ -0,0 +1,237 @@
+﻿// Copyright 2017, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const mysql = require('mysql');
+const config = require('../../config');
+const options = {
+    host: config.get('MATHSMYSQL_HOST'),
+    user: config.get('MATHSMYSQL_USER'),
+    password: config.get('MATHSMYSQL_PASSWORD'),
+    database: config.get('MATHSMYSQL_DATABASE')
+};
+//const connection = mysql.createConnection(options);
+const pool = mysql.createPool(options);
+
+function list(userId, limit, token, cb) {
+    token = token ? parseInt(token, 10) : 0;
+    pool.getConnection(function (err, connection) {
+        // Use the connection
+        connection.query(
+            'SELECT * FROM `ActAttendReg` order by id DESC LIMIT ? OFFSET ?', [limit, token],
+            (err, results) => {
+                if (err) {
+                    cb(err);
+                    return;
+                }
+                const hasMore = results.length === limit ? token + results.length : false;
+                cb(null, results, hasMore);
+                connection.release();
+            }
+        );
+    });
+}
+
+function listBy(userId, limit, token, cb) {
+    token = token ? parseInt(token, 10) : 0;
+    pool.getConnection(function (err, connection) {
+        connection.query(
+            'SELECT * FROM `ActAttendReg` WHERE `createdById` = ? LIMIT ? OFFSET ?',
+            [userId, limit, token],
+            (err, results) => {
+                if (err) {
+                    cb(err);
+                    return;
+                }
+                const hasMore = results.length === limit ? token + results.length : false;
+                cb(null, results, hasMore);
+                connection.release();
+
+            });
+    });
+}
+
+
+function listTimestampBy(userId, author, sdate,edate, limit, token, cb) {
+    token = token ? parseInt(token, 10) : 0;
+
+    pool.getConnection(function (err, connection) {
+        connection.query(
+            'SELECT * FROM `ActAttendReg` WHERE `author` = ?  and (`logDate` between ? and ? ) order by if(rootid = 0, id, rootid),parentid  LIMIT ? OFFSET ?',
+            [author,sdate,edate,limit, token],
+            (err, results) => {
+                if (err) {
+                    cb(err);
+                    return;
+                }
+                const hasMore = results.length === limit ? token + results.length : false;
+                cb(null, results, hasMore);
+                connection.release();
+
+            });
+    });
+}
+
+function listByParentid(userId, rootid, limit, token, cb) {
+    token = token ? parseInt(token, 10) : 0;
+
+    pool.getConnection(function (err, connection) {
+        connection.query(
+            'SELECT * FROM `ActAttendReg` WHERE id=? or `rootid` = ? or `parentid` = ?  LIMIT ? OFFSET ?',
+            [rootid, rootid, rootid, limit, token],
+            (err, results) => {
+                if (err) {
+                    cb(err);
+                    return;
+                }
+                const hasMore = results.length === limit ? token + results.length : false;
+                cb(null, results, hasMore);
+                connection.release();
+
+            });
+    });
+}
+function create(userId, data, cb) {
+    console.log(data);
+    
+    pool.getConnection(function (err, connection) {
+        connection.query('INSERT INTO `ActAttendReg` SET ?', data, (err, res) => {
+            if (err) {
+                cb(err);
+                return;
+            }
+            read(userId,res.insertId, cb);
+            connection.release();
+        });
+    });
+}
+
+function read(userId, id, cb) {
+    console.log(userId);
+    console.log(id);
+    pool.getConnection(function (err, connection) {
+        connection.query(
+            'SELECT * FROM `ActAttendReg` WHERE `id` = ? ', id, (err, results) => {
+                if (!err && !results.length) {
+                    err = {
+                        code: 404,
+                        message: 'Not found'
+                    };
+                }
+                if (err) {
+                    cb(err);
+                    return;
+                }
+                cb(null, results[0]);
+                connection.release();
+            });
+    });
+}
+
+function update(userId, id, data, cb) {
+    pool.getConnection(function (err, connection) {
+        connection.query(
+            'UPDATE `ActAttendReg` SET ? WHERE `id` = ?  and `createdById` = ?', [data, id, userId], (err) => {
+                if (err) {
+                    cb(err);
+                    return;
+                }
+                read(userId, id, cb);
+                connection.release();
+            });
+    });
+}
+function updateGroupStatus(rootid, status) {
+    pool.getConnection(function (err, connection) {
+        connection.query(
+            'UPDATE `ActAttendReg` SET deptlog = ? WHERE id = ? or rootid= ? ', [status, rootid, rootid], (err) => {
+                if (err) {
+                    cb(err);
+                    return;
+                }
+                connection.release();
+            });
+    });
+}
+function _delete(userId,id, cb) {
+    pool.getConnection(function (err, connection) {
+        connection.query('DELETE FROM `ActAttendReg` WHERE `id` = ?  and  `createdById` = ?',[ id, userId ],  cb);
+        connection.release();
+    });
+}
+
+module.exports = {
+    createSchema: createSchema,
+    list: list,
+    listBy: listBy,
+    listTimestampBy: listTimestampBy,
+    listByParentid: listByParentid,
+    updateGroupStatus: updateGroupStatus,
+    create: create,
+    read: read,
+    update: update,
+    delete: _delete
+};
+
+if (module === require.main) {
+    const prompt = require('prompt');
+    prompt.start();
+
+    console.log(
+        `Running this script directly will allow you to initialize your mysql
+    database.\n This script will not modify any existing tables.\n`);
+
+    prompt.get(['user', 'password'], (err, result) => {
+        if (err) {
+            return;
+        }
+        createSchema(result);
+    });
+}
+
+function createSchema(config) {
+    const connection = mysql.createConnection(extend({
+        multipleStatements: true
+    }, config));
+
+    connection.query(
+        `CREATE DATABASE IF NOT EXISTS \`deptwork\`
+      DEFAULT CHARACTER SET = 'utf8'
+      DEFAULT COLLATE 'utf8_general_ci';
+    USE \`bookshelf\`;
+    CREATE TABLE IF NOT EXISTS \`deptwork\`.\`ActAttendReg\` (
+      \`id\` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+      \`title\` VARCHAR(255) NULL,
+      \`author\` VARCHAR(255) NULL,
+      \`authorname\` VARCHAR(255) NULL,
+      \`toStaf\` VARCHAR(255) NULL,
+      \`toName\` VARCHAR(255) NULL,
+      \`logDate\` VARCHAR(255) NULL,
+      \`description\` TEXT NULL,
+      \`deptlog\` int(11) 0,
+      \`scholog\` int(11) 0,
+      \`createdBy\` VARCHAR(255) NULL,
+      \`createdById\` VARCHAR(255) NULL,
+      \`rootid\` INT UNSIGNED NULL,
+      \`parentid\` INT UNSIGNED NULL,
+    PRIMARY KEY (\`id\`));`,
+        (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log('Successfully created schema');
+            connection.end();
+        }
+    );
+}
