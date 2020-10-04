cbfe0a472b9dc5ea635fffd577082f3b2b581631
eddylin2015 <eddylin2015@gmail.com>
2020-10-04 23:43:00 +0800

    mathlive

---
+++ public/math/ml0.js
@@ -0,0 +1,37 @@
+import MathLive from '/mathlive/dist/mathlive.mjs';
+// import MathLive from 'https://unpkg.com/mathlive/dist/mathlive.min.mjs';
+var mf_arr=[ null,null,null,null,null,null,null,null];
+const mf_li = ["mlANSQT001","mlANSQTM001", "mlANSQT002",  "mlANSQTM002", "mlANSQT003", "mlANSQT004", "mlANSQTM003", "mlANSQTM004"];
+for (let i = 0; i < 8; i++) {
+    let mf__ = MathLive.makeMathField(mf_li[i], {
+        smartMode: true,
+        virtualKeyboardMode: 'manual',
+        onContentDidChange: (mf__) => {
+            let latex = mf__.$text();
+            let lx_name=(mf__.element.id.substring(2));
+            document.getElementById(lx_name).value = escapeHtml(
+                latex
+            );
+        },
+    });
+}
+
+function escapeHtml(string) {
+    return String(string).replace(/[&<>"'`=/\u200b]/g, function (
+        s
+    ) {
+        return (
+            {
+                '&': '&amp;',
+                '<': '&lt;',
+                '>': '&gt;',
+                '"': '&quot;',
+                "'": '&#39;',
+                '/': '&#x2F;',
+                '`': '&#x60;',
+                '=': '&#x3D;',
+                '\u200b': '&amp;#zws;',
+            }[s] || s
+        );
+    });
+}
