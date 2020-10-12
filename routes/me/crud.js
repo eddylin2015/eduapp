1ed15469223aef455c8a4d27ffc0c3eb05c88592
administrator@MBC.EDU.MO <administrator@MBC.EDU.MO>
2020-10-12 12:08:08 +0800

    update

---
+++ routes/me/crud.js
@@ -0,0 +1,148 @@
+// Copyright 2017, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const express = require('express');
+const images = require('./images');
+var oauth2 = require('../../db/internalOauth2.js')
+function getModel() {
+  return require(`./model-mysql-pool`);
+}
+function fmt_title(username, datestr, description) {
+  description = description.split("\n")[0];
+  datestr = datestr.length > 10 ? datestr.substring(0, 10) : datestr;
+  return username + ":" + datestr + ":" + description;
+}
+function fmt_now() {
+  var d = new Date();
+  var dstr = d.getFullYear() + "-";
+  if (d.getMonth() < 10) dstr += "0";
+  dstr += d.getMonth() + 1 + "-";
+  if (d.getDate() < 10) dstr += "0";
+  dstr += d.getDate();
+  return dstr;
+}
+function checkuser(req) {
+  if (req.user.email == "lammou@mail.mbc.edu.mo") return true;
+  if (req.user.email == "joe853.hong@mail.mbc.edu.mo") return true;
+  if (req.user.email == "cheongiekchao@mail.mbc.edu.mo") return true;
+  if (req.user.email == "fongsioman@mail.mbc.edu.mo") return true;
+  return false;
+}
+const router = express.Router();
+// Use the oauth middleware to automatically get the user's profile
+// information and expose login/logout URLs to templates.
+// Set Content-Type for all responses for these routes
+router.use((req, res, next) => {
+  res.set('Content-Type', 'text/html');
+  next();
+});
+router.get('/', (req, res, next) => {
+  let pdate='2020-09-01';
+  getModel().listForFont(pdate, 100, 0, (err, entities, cursor) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    let res_=[];
+    for(let i=0;i<entities.length;i++)
+    {
+      let row=entities[i];
+      res_.push({id:row.id,type:row.type,item:row.item})
+    }
+    res.render('me/index.pug',
+    {
+      profile: req.user,
+      itemsData: JSON.stringify(res_),
+      itemsObj:res_
+    });
+  });
+
+});
+router.get('/main.php', (req, res, next) => {
+  let pdate='2020-06-01';
+  getModel().listForFont(pdate, 100, 0, (err, entities, cursor) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    let res_=[];
+    for(let i=0;i<entities.length;i++)
+    {
+      let row=entities[i];
+      res_.push({id:row.id,type:row.type,item:row.item})
+    }
+    res.render('me/index.pug',
+    {
+      profile: req.user,
+      itemsData: JSON.stringify(res_),
+      itemsObj:res_
+
+    });
+  });
+
+});
+router.post('/getitemdata.php', (req, res, next) => {
+  getModel().listByType(req.body.t, 100, 0, (err, entities, cursor) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.end(JSON.stringify(entities));
+  });
+});
+router.get('/getitemdata.php', (req, res, next) => {
+  getModel().listByType(req.body.t, 100, 0, (err, entities, cursor) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.end(JSON.stringify(entities));
+  });
+});
+
+router.post('/getcontentdata.php', (req, res, next) => {
+  getModel().read(req.body.t, (err, entities, cursor) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    entities.detail=entities.detail.replace(/[\r\n]/g,"<br>");
+    //entities.detail=entities.detail.replace(/[ ]/g,"&nbsp;&nbsp;");
+    res.end("<p><p>"+entities.item+"<p>"+entities.detail+"<p>"+entities.item_date);
+  });
+});
+
+router.get('/getcontentdata.php', (req, res, next) => {
+  getModel().read(req.query.t, (err, entities, cursor) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    entities.detail=entities.detail.replace(/[\r\n]/g,"<br>");
+    //entities.detail=entities.detail.replace(/[ ]/g,"&nbsp;&nbsp;");
+    res.end("<p><p>"+entities.item+"<p>"+entities.detail+"<p>"+entities.item_date);
+  });
+});
+
+/**
+ * Errors on "/books/*" routes.
+ */
+router.use((err, req, res, next) => {
+  // Format error and forward to generic error handler for logging and
+  // responding to the request
+  err.response = err.message;
+  next(err);
+});
+module.exports = router;
---
+++ routes/me/model-mysql-pool.js
@@ -0,0 +1,139 @@
+﻿'use strict';
+
+const mysql = require('mysql');
+const config = require('../../config');
+const options = {
+    host: config.get('MoralEduMYSQL_HOST'),
+    user: config.get('MoralEduMYSQL_USER'),
+    password: config.get('MoralEduMYSQL_PASSWORD'),
+    database: config.get('MoralEduMYSQL_DATABASE')
+};
+//const connection = mysql.createConnection(options);
+const pool = mysql.createPool(options);
+
+function read(id,  cb) {
+    pool.getConnection(function (err, connection) {
+        connection.query(
+            'SELECT * FROM `item` WHERE `id` = ? ', id, (err, results) => {
+                if (!err && !results.length) {
+                    err = {
+                        code: 404,
+                        message: 'Not found'
+                    };
+                }
+                if (err) {
+                    cb(err);
+                    return;
+                }
+                cb(null, results[0]);
+                connection.release();
+            });
+    });
+}
+
+function listForFont(pdate, limit, token, cb) {
+    //token = token ? parseInt(token, 100) : 0;
+    limit=100;
+    token=0;
+    pool.getConnection(function (err, connection) {
+        connection.query(
+            "SELECT id,type,item FROM `item` WHERE `item_date` >= ? or `type`='slideimg'  order by id desc LIMIT ? OFFSET ?;",
+            [pdate, limit, token],
+            (err, results) => {
+                if (err) {
+                    cb(err);
+                    return;
+                }
+                //const hasMore = results.length === limit ? token + results.length : false;
+                const hasMore=true;
+                cb(null, results, hasMore);
+                connection.release();
+            });
+    });
+}
+
+function listByType(metype, limit, token, cb) {
+    //token = token ? parseInt(token, 100) : 0;
+    limit=100;
+    token=0;
+    pool.getConnection(function (err, connection) {
+        console.log(err);
+        connection.query(
+            'SELECT * FROM `item` WHERE `type` = ?  order by id desc LIMIT ? OFFSET ?',
+            [metype, limit, token],
+            (err, results) => {
+                if (err) {
+                    cb(err);
+                    return;
+                }
+                //const hasMore = results.length === limit ? token + results.length : false;
+                const hasMore=true;
+                cb(null, results, hasMore);
+                connection.release();
+            });
+    });
+}
+
+module.exports = {
+    createSchema: createSchema,
+    listByType:listByType,
+    listForFont:listForFont,
+    read: read,
+};
+
+if (module === require.main) {
+    const prompt = require('prompt');
+    prompt.start();
+
+    console.log(
+        `Running this script directly will allow you to initialize your mysql
+    database.\n This script will not modify any existing tables.\n`);
+
+    prompt.get(['user', 'password'], (err, result) => {
+        if (err) {
+            return;
+        }
+        createSchema(result);
+    });
+}
+
+function createSchema(config) {
+    const connection = mysql.createConnection(extend({
+        multipleStatements: true
+    }, config));
+
+    connection.query(
+    `CREATE DATABASE IF NOT EXISTS \`act\`
+      DEFAULT CHARACTER SET = 'utf8mb4'
+      DEFAULT COLLATE 'utf8mb4_unicode_ci';
+    USE \`act\`;
+    CREATE TABLE IF NOT EXISTS \`act\`.\`item\` (
+
+        \`id\` int(10) unsigned NOT NULL AUTO_INCREMENT,
+        \`username\` varchar(255) NOT NULL,
+        \`displayname\` varchar(255) DEFAULT NULL,
+        \`logtime\` datetime DEFAULT NULL,
+        \`rec\` varchar(255) NOT NULL,
+        \`classno\` varchar(4) NOT NULL,
+        \`seat\` int(11) NOT NULL,
+        \`stdname\` varchar(125) NOT NULL,
+        \`birthyear\` int(11) DEFAULT NULL,
+        \`groupname\` int(11) DEFAULT NULL,
+        \`description\` text,
+        \`createdTime\` datetime DEFAULT NULL,
+        \`createdBy\` varchar(255) DEFAULT NULL,
+        \`createdById\` varchar(255) DEFAULT NULL,
+        PRIMARY KEY (\`id\`),
+        UNIQUE KEY \`username_UNIQUE\` (\`username\`)
+      ) ENGINE=InnoDB DEFAULT DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci`,
+
+      
+        (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log('Successfully created schema');
+            connection.end();
+        }
+    );
+}
