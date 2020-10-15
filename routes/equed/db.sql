76b14146cdbeffcb369cf2f105d402d5d5a368cf
eddylin2015 <eddylin2015@gmail.com>
2020-10-15 13:39:14 +0800

    qiztx db table

--- routes/equed/db.sql
+++ routes/equed/db.sql
@@ -1,22 +1,25 @@
-CREATE TABLE qiz
+CREATE TABLE qiztx
 (
 `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
 `gid` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
 `qtitle` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-`qfield` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
 `qgrade` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+`qfield` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+`tx`:int(1) unsigned ,
+`acnt`:varchar(255),    /* [1,1,2,2 ] */
+`atype`:varchar(255),   /* ['num','num','txt','mat']  */
 `qizcode` text CHARACTER SET utf8mb4,
 `anscode` text CHARACTER SET utf8mb4,
 `qnote` text CHARACTER SET utf8mb4,
 `snote` text CHARACTER SET utf8mb4,
-`pflag` int(1) CHARACTER SET utf8mb4 NOT NULL,
-`pdate` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-`createbyid`  varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-`createbyname`  varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-`createdate`  varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-`modifybyid`  varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-`modifybyname`  varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-`modifydate`  varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+`pflag` int(1) CHARACTER SET utf8mb4 ,
+`pdate` varchar(255) CHARACTER SET utf8mb4 ,
+`createbyid`  varchar(255) CHARACTER SET utf8mb4 ,
+`createbyname`  varchar(255) CHARACTER SET utf8mb4 ,
+`createdate`  varchar(255) CHARACTER SET utf8mb4 ,
+`modifybyid`  varchar(255) CHARACTER SET utf8mb4 ,
+`modifybyname`  varchar(255) CHARACTER SET utf8mb4,
+`modifydate`  varchar(255) CHARACTER SET utf8mb4 ,
  PRIMARY KEY (`id`),
  UNIQUE KEY `gid_UNIQUE` (`gid`)
 )DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
