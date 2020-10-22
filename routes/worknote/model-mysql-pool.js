11a1ab728558310ee11a3d211061cf7fba60a8f9
unknown <eddylin2015@gmail.com>
2020-10-22 15:22:20 +0800

    fix

--- routes/worknote/model-mysql-pool.js
+++ routes/worknote/model-mysql-pool.js
@@ -1,25 +1,12 @@
-﻿// Copyright 2017, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-'use strict';
+﻿'use strict';
 
 const mysql = require('mysql');
-
+const config = require('../../config');
 const options = {
-    host: '127.0.0.1',
-    user: 'deptwork',
-    password: 'dw2018@#123',
-    database: 'deptwork'
+        host: config.get('DEPTWORKMYSQL_HOST'),
+        user: config.get('DEPTWORKMYSQL_USER'),
+    password: config.get('DEPTWORKMYSQL_PASSWORD'),
+    database: config.get('DEPTWORKMYSQL_DATABASE')
 };
 
 //const connection = mysql.createConnection(options);
