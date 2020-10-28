c56977efe4b71c936ce4c8fd6678b535bae8b11f
eddylin2015 <eddylin2015@gmail.com>
2020-10-28 23:15:34 +0800

    fix maths UI

--- routes/TMS/model-mysql.js
+++ routes/TMS/model-mysql.js
@@ -9,20 +9,17 @@ const options = {
     database: config.get('MATHSMYSQL_DATABASE')
 };
 const pool = mysql.createPool(options);
+
 function listQizTx(cb) {
     pool.getConnection(function (err, connection) {
         if (err) { cb(err); return; }
         connection.query(
             'SELECT id,gid,qgrade,qtitle,snote FROM `qiztx` where pflag=1 order by gid ', [],
             (err, results) => {
-                if (err) {
-                    cb(err);
-                    return;
-                }
+                if (err) { cb(err); return;  }
                 cb(null, results);
                 connection.release();
-            }
-        );
+            });
     });
 }
 function list(userId, cb) {
@@ -31,14 +28,10 @@ function list(userId, cb) {
         connection.query(
             'SELECT * FROM `reltbl` order by id DESC ', [],
             (err, results) => {
-                if (err) {
-                    cb(err);
-                    return;
-                }
+                if (err) {  cb(err); return; }
                 cb(null, results);
                 connection.release();
-            }
-        );
+            });
     });
 }
 function listByUser(username, limit,token,cb) {
@@ -48,15 +41,11 @@ function listByUser(username, limit,token,cb) {
         connection.query(
             'SELECT * FROM `reltbl` where username=? order by id DESC LIMIT ? OFFSET ?', [username, limit, token],
             (err, results) => {
-                if (err) {
-                    cb(err);
-                    return;
-                }
+                if (err) {  cb(err);  return;  }
                 const hasMore = results.length === limit ? token + results.length : false;
                 cb(null, results, hasMore);
                 connection.release();
-            }
-        );
+            });
     });
 }
 
@@ -65,10 +54,7 @@ function AddTMSQF(fn, md, jsondata, username,dname,grade,classname,seat, cb) {
     pool.getConnection(function (err, connection) {
         if (err) { cb(err); return; }
         connection.query('INSERT INTO `reltbl` SET ? ', [data], (err, res) => {
-            if (err) {
-                cb(err);
-                return;
-            }
+            if (err) {   cb(err);return;  }
             cb(null, res.insertId);
             connection.release();
         });
@@ -82,10 +68,7 @@ function TMSQFlistbydate(sd, ed,classname,limit,token, cb) {
         if(classname&&classname.length>3)  sql=`SELECT * FROM reltbl where classno='${classname}' and md >= ? and md <= ?  order by id DESC LIMIT ? OFFSET ?;`
         else if(classname&&classname.length>2)  sql=`SELECT * FROM reltbl where grade='${classname}' and md >= ? and md <= ?  order by id DESC LIMIT ? OFFSET ?;`
         connection.query(sql, [sd, ed,limit,token], function (err, results) {
-            if (err) {
-                cb(err);
-                return;
-            }
+            if (err) {cb(err);return; }
             const hasMore = results.length === limit ? token + results.length : false;
             cb(null, results, hasMore);
             connection.release();
